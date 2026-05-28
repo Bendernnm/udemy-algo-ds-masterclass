4048f2a4b7f60b5ca13e01d201b32df550d1a804
Dmytro <dmytro.olashyn@uzhnu.edu.ua>
2026-05-28 22:29:48 +0200

    add loop version

--- src/recursions/factorial/factorial.ts
+++ src/recursions/factorial/factorial.ts
@@ -6,4 +6,12 @@ export function factorial(n: number) {
   return n * factorial(n - 1);
 }
 
+export function factorialLoop(num: number) {
+  let total = 2;
 
+  for (let i = num; i > 2; i -= 1) {
+    total *= i
+  }
+
+  return total;
+}
--- src/recursions/factorial/index.ts
+++ src/recursions/factorial/index.ts
@@ -1,5 +1,7 @@
-import { factorial } from './factorial';
+import { factorial, factorialLoop } from './factorial';
 
 const factorial1 = factorial(10);
+const factorial2 = factorialLoop(10);
 
 console.log(factorial1);
+console.log(factorial2);
