61c78821337780b2fd394c1f6726c1c63e7fd795
Dmytro <dmytro.olashyn@uzhnu.edu.ua>
2026-05-28 23:06:15 +0200

    add types

--- src/recursions/power/index.ts
+++ src/recursions/power/index.ts
@@ -1,7 +1,7 @@
 import { power, powerWithHelper } from './power';
 
-const power1 = power(3, 8);
-const power2 = powerWithHelper(3, 8);
+const power1: number = power(3, 8);
+const power2: number = powerWithHelper(3, 8);
 
 console.log(power1);
 console.log(power2);
--- src/recursions/power/power.ts
+++ src/recursions/power/power.ts
@@ -1,4 +1,4 @@
-export function power(base: number, exponent: number) {
+export function power(base: number, exponent: number): number {
   if (exponent === 0) {
     return 1;
   } else if (exponent === 1) {
@@ -8,7 +8,7 @@ export function power(base: number, exponent: number) {
   return base * power(base, exponent - 1)
 }
 
-export function powerWithHelper(base: number, exponent: number) {
+export function powerWithHelper(base: number, exponent: number): number {
   if (exponent === 0) {
     return 1;
   }
